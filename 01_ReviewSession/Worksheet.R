6b5f4c60e2a80cda012cc4a8455b87f56da43db1
justinhjy1004@github.com <justinhjy1004@gmail.com>
2023-01-25 09:31:52 -0600

    stuff

---
+++ 01_ReviewSession/Worksheet.R
@@ -0,0 +1,102 @@
+#=====================================================================
+# Question 1
+# Unzip and read the csv file "food_stamp_2022_2021.csv"
+#=====================================================================
+
+
+#=====================================================================
+# Question 2
+# Get the summary statistics of Household income, Number of Foodstamps
+# and number of foodstamps
+#=====================================================================
+
+
+
+#=====================================================================
+# Question 3
+# Get the Histogram of Household income, Number of Foodstamps
+# and number of foodstamps
+#=====================================================================
+
+
+
+#=====================================================================
+# Question 4
+# Verify typeof YEAR and subset by year
+#=====================================================================
+
+
+
+
+#=====================================================================
+# Question 5
+# Plot scatterplots between Household income, Number of Foodstamps
+# and number of foodstamps
+#=====================================================================
+
+
+#=====================================================================
+# Question 6
+# Calculate the 'effective stamp' received which is defined as
+# number of months on food stamp multiplied by the number of 
+# receipients. Plot the relationship
+#=====================================================================
+
+
+
+#=====================================================================
+# Question 7
+# Write a function to for a two-sampled t-test, assuming that
+# there is no difference between the years, returning the p-value
+#=====================================================================
+
+
+t_test <- function(sample_x, sample_y) {
+  
+  #=====================================================================
+  # Question 7(a)
+  # Calculate the number of samples
+  #=====================================================================
+  
+
+  
+  #=====================================================================
+  # Question 7(b)
+  # Calculate the mean of samples
+  #=====================================================================
+  
+
+  
+  #=====================================================================
+  # Question 7(c)
+  # Calculate the standard deviation of samples
+  #=====================================================================
+  
+
+  
+  #=====================================================================
+  # Question 7(d)
+  # Calculate the degrees of freedom, standard deviation of the population,
+  # the t-statistic and the p-value
+  #===================================================================== 
+  
+
+  return(p_val)
+}
+
+#=====================================================================
+# Question 8
+# Calculate the p-value using t_test (your function) and 
+# t.test (R's base function) to see if the number of receipients of 
+# foodstamps and the duration of receipt are statistically different
+#===================================================================== 
+
+
+
+#=====================================================================
+# Question 9
+# What can you say about the observation?
+#===================================================================== 
+
+
+
